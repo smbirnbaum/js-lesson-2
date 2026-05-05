e6ec7ebaaafa5403205a1f2f994a68c4aaba6d47
korvmacka <shawnabirnbaum@email.arizona.edu>
2026-05-05 09:28:40 +0200

    Initial commit

---
+++ l2.js
@@ -0,0 +1,16 @@
+function isValidPassword(password, username) {
+    if (password.length < 8) {
+        return false;
+    }
+
+    if (password.includes(" ")) {
+        return false;
+    }
+
+    if (password.includes(username)) {
+        return false;
+    }
+
+    return true;
+}
+
